c513a4b6d5c1e1026217c07d7e14a6d019be210b
n.bibik <osbib2@gmail.com>
2018-05-03 01:38:20 +0300

    Removed old ListFragment.

--- app/src/main/java/org/donampa/nbibik/dipl/SplashActivity.java
+++ app/src/main/java/org/donampa/nbibik/dipl/SplashActivity.java
@@ -1,11 +1,11 @@
 package org.donampa.nbibik.dipl;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
